18c8f57998d4f9b1d26510743edefef17dcca146
Bryan Jiang <bryanjiang7@gmail.com>
2020-06-27 10:40:36 -0500

    2006271040 tidy up

--- Properties.h
+++ Properties.h
@@ -18,21 +18,22 @@ static const std::string PIECES = "RNBQKBNRYYYYYYYY";
 
 // Greet the players.
 static const std::string GREETING =
-"Welcome to Chess!\n\n" +
+"Welcome to Terminal Chess!\n\n" +
 PLAYER_WHITE + " will be uppercase.\n" +
 PLAYER_BLACK + " will be lowercase.\n\n"
 "Moves must be made like this: e2e4, e7e5, g1f3, etc.\n\n"
 "Enter /C to view list of valid commands.\n\n";
 
+
 //////////////////////////////// DO NOT MODIFY BELOW ////////////////////////////////
 static std::string CURRENT_PLAYER = PLAYER_WHITE; // keep track of current player; White moves first.
 static const std::string ALPHABET = "abcdefghijklmnopqrstuvwxyz"; // label the files
-static const int NUM_EMPTY_SPACES = NUM_RANKS * NUM_FILES - PIECES.size() * 2;
-static const std::string EXIT_CODES =
+static const int NUM_EMPTY_SPACES = NUM_RANKS * NUM_FILES - PIECES.size() * 2; // number of empty squares at start of game
+static const std::string EXIT_CODES = // what the program should return
 "Exit codes:\n"
 " 0 : Program exited successfully.\n"
 "-1 : Program exited with an error.\n";
-static const std::string COMMAND_CODES =
+static const std::string COMMAND_CODES = // commands player can make during their turn
 "Enter any of the following to execute its respective command. Case sensitive.\n"
 "/f : Flip the board.\n"
 "/r : Resign.\n"
